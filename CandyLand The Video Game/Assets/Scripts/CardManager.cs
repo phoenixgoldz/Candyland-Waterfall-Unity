1456afb251b88df988c1d2c58cf0083ffe24d285
JohnBeck19 <jbeck@student.neumont.edu>
2025-01-27 09:51:24 -0700

    card images

--- CandyLand The Video Game/Assets/Scripts/CardManager.cs
+++ CandyLand The Video Game/Assets/Scripts/CardManager.cs
@@ -1,8 +1,12 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-
+    [SerializeField] public List<Sprite> cardImagesSingle;
+    [SerializeField] public List<Sprite> cardImagesDouble;
+    [SerializeField] public List<Sprite> cardImagesSpecial;
     void Start()
     {
         
@@ -17,7 +21,7 @@ public class CardManager : MonoBehaviour
     public card drawCard()
     { 
         card card = new card();
-        if (Random.Range(0,100 ) <= -10)
+        if (Random.Range(0,100 ) <= 10)
         {
             card.color = TILE_TYPE.SPECIAL;
             card.special = (SPECIAL_TYPE)Random.Range(0, 5);
@@ -31,8 +35,79 @@ public class CardManager : MonoBehaviour
                 card.doubleCard = true; 
             }
         }
-       
-        
+
+        if (card.doubleCard)
+        {
+            switch (card.color)
+            {
+                case TILE_TYPE.RED:
+                    card.cardImg = cardImagesDouble[0];
+                    break;
+                case TILE_TYPE.PURPLE:
+                    card.cardImg = cardImagesDouble[1];
+                    break;
+                case TILE_TYPE.YELLOW:
+                    card.cardImg = cardImagesDouble[2];
+                    break;
+                case TILE_TYPE.BLUE:
+                    card.cardImg = cardImagesDouble[3];
+                    break;
+                case TILE_TYPE.ORANGE:
+                    card.cardImg = cardImagesDouble[4];
+                    break;
+                case TILE_TYPE.GREEN:
+                    card.cardImg = cardImagesDouble[5];
+                    break;
+            }
+        }
+        else if (card.special != SPECIAL_TYPE.NONE)
+        {
+            switch (card.special)
+            {
+                case SPECIAL_TYPE.GINGERBREAD:
+                    card.cardImg = cardImagesSpecial[0];
+                    break;
+                case SPECIAL_TYPE.CANDYCANE:
+                    card.cardImg = cardImagesSpecial[1];
+                    break;
+                case SPECIAL_TYPE.LOLLIPOP:
+                    card.cardImg = cardImagesSpecial[2];
+                    break;
+                case SPECIAL_TYPE.GUMDROP:
+                    card.cardImg = cardImagesSpecial[3];
+                    break;
+                case SPECIAL_TYPE.ICECREAM:
+                    card.cardImg = cardImagesSpecial[4];
+                    break;
+                case SPECIAL_TYPE.PEANUT:
+                    card.cardImg = cardImagesSpecial[5];
+                    break;
+            }
+        }
+        else
+        {
+            switch (card.color)
+            {
+                case TILE_TYPE.RED:
+                    card.cardImg = cardImagesSingle[0];
+                    break;
+                case TILE_TYPE.PURPLE:
+                    card.cardImg = cardImagesSingle[1];
+                    break;
+                case TILE_TYPE.YELLOW:
+                    card.cardImg = cardImagesSingle[2];
+                    break;
+                case TILE_TYPE.BLUE:
+                    card.cardImg = cardImagesSingle[3];
+                    break;
+                case TILE_TYPE.ORANGE:
+                    card.cardImg = cardImagesSingle[4];
+                    break;
+                case TILE_TYPE.GREEN:
+                    card.cardImg = cardImagesSingle[5];
+                    break;
+            }
+        }
 
         return card;
     }
--- CandyLand The Video Game/Assets/Scripts/card.cs
+++ CandyLand The Video Game/Assets/Scripts/card.cs
@@ -5,14 +5,6 @@ public class card : MonoBehaviour
     [SerializeField] public TILE_TYPE color;
     [SerializeField] public SPECIAL_TYPE special;
     [SerializeField] public bool doubleCard;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] public Sprite cardImg;
 }
